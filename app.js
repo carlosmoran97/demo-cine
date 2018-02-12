1e8a25ded878f8b2acc730db89551a4d114904ec
Carlos Morán <moran_krlos@hotmail.com>
2018-02-12 13:48:39 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const Readline = require('readline');
+const rl = Readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+});
+rl.setPrompt('> ');
+rl.prompt();
+
+rl.on('line', reply => {
+    rl.prompt();
+});
